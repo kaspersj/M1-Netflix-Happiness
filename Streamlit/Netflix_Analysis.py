d6c0785104075f0bbf2f8b0ca452e19836183dbb
John <kaspersj@umich.edu>
2022-10-06 18:10:30 +0000

    Sample chart

--- Streamlit/Netflix_Analysis.py
+++ Streamlit/Netflix_Analysis.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
 st.title('Our Netflix Analysis')
+
+x = np.arange(100)
+source = pd.DataFrame({
+  'x': x,
+  'f(x)': np.sin(x / 5)
+})
+
+example_chart = alt.Chart(source).mark_line().encode(
+    x='x',
+    y='f(x)'
+)
+
+st.DataFrame(source)
+
+st.altair_chart(example_chart)
