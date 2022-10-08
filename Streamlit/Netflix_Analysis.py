860f955d3b06666c864058c7b5917aef97db762f
John <kaspersj@umich.edu>
2022-10-08 17:50:31 +0000

    Adjusting names  in streamlit

--- Streamlit/Netflix_Analysis.py
+++ Streamlit/Netflix_Analysis.py
@@ -5,6 +5,21 @@ import altair as alt
 
 st.title('Our Netflix Analysis')
 
+st.header("John Kaspers (kaspersj), \
+            Ong Hock Boon Steven David (steveong), \
+            Chi Huen Fong (chfong)
+ ")
+st.markdown('\n\n')
+st.header("Supplementary visualizations - some are interactive!")
+st.markdown('\n\n')
+
+# Data Sources
+# Netflix Pricing
+# https://www.comparitech.com/blog/vpn-privacy/countries-netflix-cost/
+# World Happiness Report
+# https://www.kaggle.com/datasets/ajaypalsinghlo/world-happiness-report-2021
+
+
 x = np.arange(100)
 source = pd.DataFrame({
   'x': x,
@@ -19,3 +34,6 @@ example_chart = alt.Chart(source).mark_line().encode(
 st.dataframe(source)
 
 st.altair_chart(example_chart)
+
+
+
